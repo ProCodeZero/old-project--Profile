0e22d78d2e0230a97bcfc3918191388341b130e2
HollowDS <denis.ospov.2017@inbox.ru>
2023-10-21 12:30:13 +0300

    fix popup menu for mobile version

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@ $(".scrollto a").on("click", function () {
 	let href = $(this).attr("href");
 
 	$("html, body").animate({
-			scrollTop: $(href).offset().top
+		scrollTop: $(href).offset().top
 	}, {
-			duration: 370,   // по умолчанию «400»
-			easing: "linear" // по умолчанию «swing»
+		duration: 370,   // по умолчанию «400»
+		easing: "linear" // по умолчанию «swing»
 	});
 
 	return false;
@@ -15,112 +15,41 @@ $("a[href^='#']").on("click", function () {
 	let href = $(this).attr("href");
 
 	$("html, body").animate({
-			scrollTop: $(href).offset().top
+		scrollTop: $(href).offset().top
 	});
 
 	return false;
 });
 
-// Popup
-const popupLinks = document.querySelectorAll(".popup-link");
+// Popup variables
+const popupCloseIcon = document.querySelector(".close-popup");
+const popupLink = document.querySelector(".popup-link");
+const popup = document.querySelector(".popup");
 const body = document.querySelector("body");
-// Для выравнивания фиксированных объектов
-const lockPadding = document.querySelectorAll("lock-padding");
-//
-let unlock = true;
-const timeout = 800;
+console.log('popupCloseIcon :>> ', popupCloseIcon);
+console.log('popupLink :>> ', popupLink);
+console.log('body :>> ', body);
 
 // Popup code
-if (popupLinks.length > 0) {
-  for (let index = 0; index < popupLinks.length; index++) {
-    const popupLink = popupLinks[index];
-    popupLink.addEventListener("click", function (e) {
-      const popupName = popupLink.getAttribute("href").replace("#", "");
-      const curentPopup = document.getElementById(popupName);
-      popupOpen(curentPopup);
-      e.preventDefault();
-    });
-  }
-}
-const popupCloseIcon = document.querySelectorAll(".close-popup");
-if (popupCloseIcon.length > 0) {
-  for (let i = 0; i < popupCloseIcon.length; i++) {
-    const el = popupCloseIcon[i];
-    el.addEventListener("click", function (e) {
-      popupClose(el.closest(".popup"));
-      e.preventDefault();
-    });
-  }
-}
-function popupOpen(curentPopup) {
-  if (curentPopup && unlock) {
-    const popupActive = document.querySelector(".popup.open");
-    if (popupActive) {
-      popupClose(popupActive, false);
-    } else {
-      // Отключение скрола основной страницы + убирание ползунка
-      // bodyLock();
-    }
-    curentPopup.classList.add("open");
-    curentPopup.addEventListener("click", function (e) {
-      if (!e.target.closest(".popup__content")) {
-        popupClose(e.target.closest(".popup"));
-      }
-    });
-  }
-}
-
-function popupClose(popupActive, doUnlock = true) {
-  if (unlock) {
-    popupActive.classList.remove("open");
-    if (doUnlock) {
-      // Возвращение скрола основной страницы + убирание ползунка
-      // bodyUnlock();
-    }
-  }
-}
-// Отключение скрола основной страницы + убирание ползунка
-function bodyLock() {
-  const lockPaddingValue =
-    window.innerWidth - document.querySelector(".wrapper").offsetWidth + "px";
-
-  if (lockPadding.length > 0) {
-    for (let i = 0; i < lockPadding.length; i++) {
-      const el = lockPadding[i];
-      el.style.paddingRight = lockPaddingValue;
-    }
-  }
-  body.style.paddingRight = lockPaddingValue;
-  body.classList.add("lock");
-
-  unlock = false;
-  setTimeout(() => {
-    unlock = true;
-  }, timeout);
-}
-// Возвращение скрола основной страницы + убирание ползунка
-function bodyUnlock() {
-  setTimeout(() => {
-    if (lockPadding.length > 0) {
-      for (let i = 0; i < lockPadding.length; i++) {
-        const el = lockPadding[i];
-        el.computedStyleMap.paddingRight = "0px";
-      }
-    }
-    body.style.paddingRight = "0px";
-    body.classList.remove("lock");
-  }, timeout);
-
-  unlock = false;
-  setTimeout(() => {
-    unlock = true;
-  }, timeout);
-}
-
-document.addEventListener("keydown", function (e) {
-  if (e.which === 27) {
-    const popupActive = this.document.querySelector("popup.open");
-    popupClose(popupActive);
-  }
+popupLink.addEventListener("click", function (e) {
+	popupOpen(popup);
+	e.preventDefault();
 });
+
+popupCloseIcon.addEventListener("click", function (e) {
+	popupClose(popup);
+	e.preventDefault();
+});
+
+function popupOpen(curentPopup) {
+	curentPopup.classList.add("open");
+	popupLink.style["display"] = "none"
+	popupCloseIcon.style["display"] = "block"
+}
+
+function popupClose(popupActive) {
+	popupActive.classList.remove("open");
+	popupLink.style["display"] = "block"
+	popupCloseIcon.style["display"] = "none"
+}
 // End popup code
